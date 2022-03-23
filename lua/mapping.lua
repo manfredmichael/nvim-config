1143f536e202b1a989ee6b61a756945348200144
manfred michael <koskosanrider@gmail.com>
2022-03-23 07:39:59 +0700

    Add telescope keymap

--- lua/mapping.lua
+++ lua/mapping.lua
@@ -4,4 +4,4 @@ local map = vim.api.nvim_set_keymap
 map('n', '<leader>e', ':NERDTreeToggle<CR>', {noremap = true, silent = true})
 map('n', '<leader>r', ':ToggleTerm<CR>', {noremap = true, silent = true})
 
-
+map('n', '<leader>ff', ':Telescope find_files', {noremap = true, silent = true})
