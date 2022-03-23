6e70bfa8c622698840243c83d8dc247d2859ed32
manfred michael <koskosanrider@gmail.com>
2022-03-23 19:20:16 +0700

    Add fugitive keymaps (It's crazy! I love it)

--- lua/mapping.lua
+++ lua/mapping.lua
@@ -3,5 +3,8 @@ local map = vim.api.nvim_set_keymap
 
 map('n', '<leader>e', ':NERDTreeToggle<CR>', {noremap = true, silent = true})
 map('n', '<leader>r', ':ToggleTerm<CR>', {noremap = true, silent = true})
-
 map('n', '<leader>f', ':Telescope find_files<CR>', {noremap = true, silent = true})
+
+
+map('n', '<leader>g', ':G<CR>', {noremap = true, silent = true})
+map('n', '<leader>gc', ':Git commit<CR>', {noremap = true, silent = true})
