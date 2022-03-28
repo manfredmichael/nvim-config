d3ecb997f1e0c074b1ffa8fbbc56e76b93cecb42
manfred michael <koskosanrider@gmail.com>
2022-03-28 07:35:48 +0700

    Fix need to press esc on startup

--- lua/autocmd.lua
+++ lua/autocmd.lua
@@ -1,9 +1,11 @@
 -- Run ToggleTerm on start up 
 vim.api.nvim_exec([[
     autocmd VimEnter * ToggleTerm size=10 
-  ]], false)
+    autocmd VimEnter * ToggleTerm size=10 
+    autocmd VimEnter * ToggleTerm size=10 
+  ]], true)
 
 -- Run NERDTree on start up 
 vim.api.nvim_exec([[
     autocmd VimEnter * NERDTree
-  ]], false)
+  ]], true)
