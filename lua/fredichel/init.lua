bc24bbcdedb52e48acca9bc35c8d126f1b51810c
manfred michael <koskosanrider@gmail.com>
2023-04-06 14:24:37 +0700

    Add fugitive and packer

--- lua/fredichel/init.lua
+++ lua/fredichel/init.lua
@@ -1,3 +1,2 @@
 require("fredichel.remap")
 require("fredichel.packer")
-print("Hello from manfred")
--- lua/fredichel/packer.lua
+++ lua/fredichel/packer.lua
@@ -17,6 +17,7 @@ return require('packer').startup(function(use)
   use('nvim-treesitter/nvim-treesitter', {run= ':TSUpdate'})
   use('nvim-treesitter/playground')
   use('mbbill/undotree')
+  use('theprimeagen/harpoon')
   use('tpope/vim-fugitive')
   
 
